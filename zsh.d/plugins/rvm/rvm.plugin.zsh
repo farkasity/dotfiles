cdeeaaf11425db1e1ae04047817141afde707a2e
zhustec <zhustec@foxmail.com>
2020-04-05 20:19:02 +0800

    remove rvm completion
    Signed-off-by: zhustec <zhustec@foxmail.com>

--- zsh.d/plugins/rvm/rvm.plugin.zsh
+++ zsh.d/plugins/rvm/rvm.plugin.zsh
@@ -1,17 +1,3 @@
-RVM_PATH="${RVM_PATH:-$HOME/.rvm}"
-
-export PATH="$RVM_PATH/bin:$PATH"
-
-[ -f "$RVM_PATH/scripts/rvm" ] && source "$RVM_PATH/scripts/rvm"
-
-function _rvm() {
-    if [ -f "$RVM_PATH/scripts/zsh/Completion/_rvm" ]; then
-        source "$RVM_PATH/scripts/zsh/Completion/_rvm"
-    fi
-}
-
-compdef _rvm rvm
-
 alias rvd="rvm current"
 alias rvd="rvm use default"
 alias rvd="rvm use default"
